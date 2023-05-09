28d655a9acc5142bfcf5215ecbe6d3273b93ff3f
HarshSathwara1999 <132983820+HarshSathwara1999@users.noreply.github.com>
2023-05-09 15:52:18 +0530

    Which item was purchased just before the customer became a member?

---
+++ Que-7.sql
@@ -0,0 +1,13 @@
+---Which item was purchased just before the customer became a member?
+
+With A as
+(
+Select  S.customer_id,M.product_name,S.Order_date,
+	Dense_rank() OVER (Partition by S.Customer_id Order by S.Order_date) as RANK
+From Sales S Join Menu M ON m.product_id = s.product_id JOIN Members Mem
+ON Mem.Customer_id = S.customer_id
+Where S.order_date <= Mem.join_date  
+)
+Select *
+From A
+Where RANK = 1
